f521c14587423fa068bad5c3b7c3048d1bba06db
Seyon <seyontennis@gmail.com>
2026-01-29 14:40:43 +0100

    test

--- doc/conf.py
+++ doc/conf.py
@@ -35,7 +35,7 @@ epub_show_urls = 'footnote'
 # ones.
 extensions = [
     'sphinx.ext.duration',
-    'sphinxcontrib.video'
+    #'sphinxcontrib.video'
 ]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
